87aa4a5c2bf331863280f8d21f55863eb3e4956a
jac267 <jackpotchap@gmail.com>
2023-05-18 12:03:05 -0400

    background dynamic

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,21 @@
 function Background(params) {
   return (
-    <div id="backgroundSolidColor">
-      <div id="backgroundBriks"></div>
+    <div class="background">
+      <div class="backgroundSolidColor"></div>
+      <div class="backgroundBriks"></div>
       <div
-        id="backgroundFilter"
+        class="backgroundFilter"
         style={{ backgroundImage: params.backgroundImage }}
       ></div>
+
+      <div class="backgroundFILL">
+        <div class="backgroundSolidColorFILL"></div>
+        <div class="backgroundBriksFILL"></div>
+        <div
+          class="backgroundFilterFILL"
+          style={{ backgroundColor: params.backgroundColor }}
+        ></div>
+      </div>
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,26 @@
 function Home() {
   return (
-    <Background backgroundImage="url('../public/images/staticImages/backgroundPurpleFilter.png')" />
+    <div id="homePage">
+      <Background
+        backgroundImage="url('../public/images/staticImages/backgroundPurpleFilter.png')"
+        backgroundColor="rgba(9, 9, 25, 0.8)"
+      />
+
+      <div id="homePageMenu">
+        <NeonIcon backgroundImage="url('../public/images/staticImages/neonCrayon.png')" />
+        <NeonIcon backgroundImage="url('../public/images/staticImages/neonMoney.png')" />
+        <NeonIcon backgroundImage="url('../public/images/staticImages/neonCalculet.png')" />
+        <NeonIcon backgroundImage="url('../public/images/staticImages/neonStonks.png')" />
+      </div>
+    </div>
+  );
+}
+
+function NeonIcon(params) {
+  return (
+    <button
+      class="neon-icon"
+      style={{ backgroundImage: params.backgroundImage }}
+    ></button>
   );
 }
