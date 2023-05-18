c977bfee64b7940dafd4d7c57a14e61cc1362260
jac267 <jackpotchap@gmail.com>
2023-05-18 15:55:32 -0400

    menu pas clikable

--- src/components/index.js
+++ src/components/index.js
@@ -7,36 +7,56 @@ function Home() {
       />
 
       <div id="homePageMenu">
-        <NeonIcon backgroundImage="../public/images/staticImages/neonCrayon.png" />
-        <NeonIcon backgroundImage="../public/images/staticImages/neonMoney.png" />
-        <NeonIcon backgroundImage="../public/images/staticImages/neonCalculet.png" />
-        <NeonIcon backgroundImage="../public/images/staticImages/neonStonks.png" />
+        <NeonIcon
+          backgroundImage="../public/images/staticImages/neonCrayon.png"
+          id="neonCrayon"
+        />
+        <NeonIcon
+          id="neonMoney"
+          backgroundImage="../public/images/staticImages/neonMoney.png"
+        />
+        <NeonIcon
+          backgroundImage="../public/images/staticImages/neonCalculet.png"
+          id="neonCalculet"
+        />
+        <NeonIcon
+          backgroundImage="../public/images/staticImages/neonStonks.png"
+          id="neonStonks"
+        />
 
-        <img src="../public/images/staticImages/cheque.png"></img>
+        <div class="menu-button-div" id="cheque-div">
+          <img src="../public/images/staticImages/cheque.png" id="cheque"></img>
+        </div>
         <MenuButton
-          backgroundImage="../public/images/staticImages/l’APRÈS D.E.C..png"
+          backgroundImage="url('../public/images/staticImages/l’APRÈS D.E.C..png')"
           nbSpam="6"
+          id="apres-D-E-C"
         />
 
         <MenuButton
-          backgroundImage="../public/images/staticImages/PARTIR UNE ENTREPRISE.png"
+          backgroundImage="url('../public/images/staticImages/PARTIR UNE ENTREPRISE.png')"
           nbSpam="4"
+          id="partir-une-entreprise"
         />
         <MenuButton
-          backgroundImage="../public/images/staticImages/LES CONCEPTS COMPLEXES.png"
+          backgroundImage="url('../public/images/staticImages/LES CONCEPTS COMPLEXES.png')"
           nbSpam="5"
+          id="les-concepts-complexes"
         />
         <MenuButton
-          backgroundImage="../public/images/staticImages/SUITE OFFICE.png"
+          backgroundImage="url('../public/images/staticImages/SUITE OFFICE.png')"
           nbSpam="9"
+          id="suite-office"
         />
         <MenuButton
-          backgroundImage="../public/images/staticImages/LES BASES.png"
+          backgroundImage="url('../public/images/staticImages/LES BASES.png')"
           nbSpam="8"
+          id="les-bases"
         />
         <MenuButton
-          backgroundImage="../public/images/staticImages/FINANCES PERSO- NELLES.png"
+          backgroundImage="url('../public/images/staticImages/FINANCES PERSO- NELLES.png')"
           nbSpam="8"
+          id="finances-personelles"
         />
       </div>
     </div>
@@ -45,20 +65,19 @@ function Home() {
 
 function NeonIcon(params) {
   return (
-    <button class="neon-icon">
+    <button class="neon-icon" id={params.id}>
       <img src={params.backgroundImage}></img>
     </button>
   );
 }
 function MenuButton(params) {
   return (
-    <div>
-      <button class="menu-button">
-        <img src={params.backgroundImage}></img>
-      </button>
-      {createMenuButton(params.nbSpam).map((spam) => (
-        <spam class={spam.props.class}></spam>
-      ))}
+    <div
+      class="menu-button-div"
+      id={params.id}
+      style={{ backgroundImage: params.backgroundImage }}
+    >
+      <button class="menu-button" onClick={() => alert(params.id)}></button>
     </div>
   );
 }
